276c5714d8e25e57478749b4535c94a41360baec
Ole Christian Langfjæran <judoole@gmail.com>
2013-06-21 16:02:32 +0200

    [Heroku] Trying a Heroku deploy.

--- site/src/ratpack/ratpack.groovy
+++ site/src/ratpack/ratpack.groovy
@@ -16,7 +16,6 @@ ratpack {
             def xml = new XmlSlurper().parse("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.sio.no%2Fwps%2Fportal%2F%3FWCM_GLOBAL_CONTEXT%3D%2Fwps%2Fwcm%2Fconnect%2Fmigration%2Fsio%2Fmat%2Bog%2Bdrikke%2Fdagens%2Bmiddag%2F"+ cantina.urlId + "'and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22sioArticleBodyText%22%5D'")
             def body = new StreamingMarkupBuilder().bindNode(xml.results).toString()
 
-            //get(TemplateRenderer).render "cantina.html", title: cantina.name, body: body
             get(TemplateRenderer).render "cantina.html", title: cantina.name, body: body
         }
     }
